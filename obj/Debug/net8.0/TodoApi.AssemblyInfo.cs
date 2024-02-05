5a5e1fd0789f776ccc13fe229b4919f16e9c4075
shad0wuk <mrsgtshadow@gmail.com>
2024-02-05 16:01:27 +0000

    Scaffold a controller

--- obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23e7611206d7491956b74bc8e2a0646344cbbe54")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7d56ce491fb297abcf99a47669dab6f0056aa12")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
